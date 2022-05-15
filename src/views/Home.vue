57540bad472fd3eace6d0ff428f052735358b940
salemkode <salem.m.s2030@gmail.com>
2022-05-15 07:44:55 +0300

    replice LastTranction with info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,76 @@
 <template>
   <div class="home">
     <Hero />
-    <LastTranction />
+    <div class="helper-info container">
+      <h2 class="mb-3">Explorer pages</h2>
+      <div v-for="(item, key) in infoUrls" :key="key" class="my-4">
+        <h3>{{ key }} Page</h3>
+        <div>
+          <p class="m-0">
+            <code>
+              {{ item.title }}
+            </code>
+            For example
+          </p>
+          <app-link :to="item.url"> {{ item.url }} </app-link>
+        </div>
+      </div>
+    </div>
     <Exchanges />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Hero from "@/components/home/Hero.vue";
-import LastTranction from "@/components/home/LastTranction.vue";
-import Exchanges from "@/components/home/Exchanges.vue";
+import { defineComponent, reactive } from "vue";
 
+// Components
+import Hero from "@/components/home/Hero.vue";
+import Exchanges from "@/components/home/Exchanges.vue";
+import AppLink from "@/components/global/AppLink.vue";
+
+//
 export default defineComponent({
   name: "Home",
-  components: { Hero, LastTranction, Exchanges },
+  components: { Hero, Exchanges, AppLink },
+  setup() {
+    const infoUrls = reactive<{
+      [key: string]: { title: string; url: string };
+    }>({
+      Token: {
+        title: "/token/:tokenid",
+        url: "/token/7a427a156fe70f83d3ccdd17e75804cc0df8c95c64ce04d256b3851385002a0b",
+      },
+      Transaction: {
+        title: "/tx/:txid",
+        url: "/tx/7a427a156fe70f83d3ccdd17e75804cc0df8c95c64ce04d256b3851385002a0b",
+      },
+      Address: {
+        title: "/address/:address",
+        url: "/address/simpleledger:qzmd5vxgh9m22m6fgvm57yd6kjnjl9qnwyztz2p80d",
+      },
+    });
+
+    return {
+      infoUrls,
+    };
+  },
 });
 </script>
+
+<style scoped>
+.helper-info * {
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+code {
+  display: block;
+  margin: 5px 0;
+  padding: 10px;
+  border-radius: 4px;
+  background: #41444e;
+  font-family: Source Code Pro, monospace;
+  font-weight: 600;
+  color: #fff;
+}
+</style>

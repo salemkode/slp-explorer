36375fc6f254609904d0d708cbe6ec459e5d1cfd
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2022-06-16 11:54:26 +0300

    Update numberWithCommas function

--- src/modules/utilities.ts
+++ src/modules/utilities.ts
@@ -20,7 +20,7 @@ export function numberWithCommas(input: string | number): string {
   const parts = input.toString().split(".");
 
   //
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  parts[0] = (+parts[0]).toLocaleString();
 
   //
   return parts.join(".");
